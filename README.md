46298481aa2a5483fac6212b279ae5e33a80951c
pamelasb <pamelasb@ciandt.com>
2019-09-24 11:18:35 -0300

    remove node_modules and add it to gitignore

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ first create a server.js file and define the http module and the port
 
 execute the npm init command to make it a node project
 
-then execute npm install express command
+then execute npm install express command (@4.16.3 -save-exact) optional
--- server.js
+++ server.js
@@ -1,15 +1,32 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(function (req, resp){
-    resp.end(`
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Node training </h1>
-        </body> 
-    </html>
-`);
+app.listen(3000, function(){
+    console.log("running server on port 3000");
 });
-server.listen(3000);
+
+app.get('/', function(req, resp){
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Node training </h1>
+            </body> 
+        </html>
+    `);
+});
+
+app.get('/books', function(req, resp){
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Books list </h1>
+            </body> 
+        </html>
+    `);
+});
